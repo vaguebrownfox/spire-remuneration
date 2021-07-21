1e448da1f5b4b0528b59a06d00e11329e1b7b2fc
vaguebrownfox <foxjournal.nsfw@gmail.com>
2021-07-21 14:30:36 +0530

    set register logic refine

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -2,12 +2,10 @@ import { db } from "./firebase";
 
 const { REGISTER } = require("./firebaseConfig");
 
-export const invitesQuery = (id) =>
-	db.collection(REGISTER).where("sender", "==", id);
-
-export const getRegisterRef = (email) => db.collection(REGISTER).doc(email); // ! keep
+export const getRegisterRef = (email) => db.collection(REGISTER).doc(email);
 
 export const setRegister = async (form) => {
+	let res = false;
 	const inviteRef = db.collection(REGISTER).doc(form.email);
 	await inviteRef
 		.set({
@@ -16,9 +14,11 @@ export const setRegister = async (form) => {
 			verified: false,
 		})
 		.then(() => {
-			//
+			res = true;
 		})
 		.catch((e) => {
 			console.log("firestore ::set register error", e);
+			res = false;
 		});
+	return res;
 };
