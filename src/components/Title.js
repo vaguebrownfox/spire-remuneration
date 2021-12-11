e790cacabb0cc7a52604efa7b0f237e157c14c3b
vaguebrownfox <foxjournal.nsfw@gmail.com>
2021-12-11 17:32:36 +0530

    user id check

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,71 +1,75 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Button, Typography } from "@material-ui/core";
+import { Button, CircularProgress, Typography } from "@material-ui/core";
+
+const WEBAPP_LINK = "https://asquire.web.app";
 
 const Title = ({ userid, username }) => {
 	const classes = useStyles();
+
+	const [wait, setWait] = React.useState(true);
+
+	React.useEffect(() => {
+		setTimeout(() => setWait(false), 5000);
+	}, []);
 	return (
 		<div className={classes.title}>
-			{userid ? (
-				<>
-					<Typography
-						variant="h5"
-						component="div"
-						align="center"
-						gutterBottom
-					>
-						Thank you{" "}
-						<span className={classes.spn}>
-							<b>{username}</b>
-						</span>{" "}
-						for participating in our research!
-					</Typography>
-					<Typography
-						variant="h6"
-						component="div"
-						align="center"
-						gutterBottom
-					>
-						Please enter your{" "}
-						<span className={classes.spn}>
-							<b>Email ID</b>
-						</span>
-					</Typography>
-				</>
-			) : (
-				<>
-					<Typography
-						variant="h4"
-						component="div"
-						align="center"
-						gutterBottom
-					>
-						{getGreet()}
-						<br />
-						Please visit{" "}
-						<span className={classes.spn}>
-							<b>Asquire</b>
-						</span>{" "}
-						project Web-app for participating in our research!
-					</Typography>
-					<Button
-						className={classes.btn}
-						variant="outlined"
-						color="secondary"
-						href="https://asquire-mox.web.app"
-					>
-						Go to Web App
-					</Button>
-					<Typography
-						variant="h6"
-						component="div"
-						align="center"
-						gutterBottom
-					>
-						Thank you!
-					</Typography>
-				</>
-			)}
+			<>
+				<Typography
+					variant="h4"
+					component="div"
+					align="center"
+					gutterBottom
+				>
+					<br />
+					{wait ? (
+						<>
+							{getGreet()}
+							<br />
+							Please wait while app is verifying your{" "}
+							<span className={classes.spn}>
+								<b>Asquire USER ID</b>
+							</span>
+							<br />
+							<div>
+								<CircularProgress
+									style={{ margin: 32 }}
+									color="secondary"
+									size={28}
+								/>
+							</div>
+						</>
+					) : (
+						<>
+							{`Hmmm... no user ID found! `}
+							<br />
+							<br />
+							{`Please visit `}
+							<span className={classes.spn}>
+								<b>{`Asquire`}</b>
+							</span>
+							{` project Web-app for participating in our research!`}
+							<br />
+						</>
+					)}
+				</Typography>
+				<Button
+					className={classes.btn}
+					variant="outlined"
+					color="secondary"
+					href={WEBAPP_LINK}
+				>
+					Go to Web-App
+				</Button>
+				<Typography
+					variant="h6"
+					component="div"
+					align="center"
+					gutterBottom
+				>
+					Thank you!
+				</Typography>
+			</>
 		</div>
 	);
 };
